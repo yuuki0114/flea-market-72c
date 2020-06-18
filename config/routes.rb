0155dfeb37a40e0c3856659e1a63bee98acecad1
naohirooda <perfect9nine0303@icloud.com>
2020-06-18 22:39:51 +0900

    ルートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   root 'items#index'
+  get 'buyers/index'
+  get 'buyers/done'
+  root 'items#index'
+  resources :items do
+    resources :buyers, only: [:index] do
+      collection do
+        get 'done', to: 'buyers#done'
+        post 'pay', to: 'buyer#pay'
+      end
+    end
+  end
 end
