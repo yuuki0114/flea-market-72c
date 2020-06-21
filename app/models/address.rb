224359f3d418f4dbe1be9409ea2d20c8b42461b2
Takuto8786 <iruka8786@gmail.com>
2020-06-21 13:45:32 +0900

    user, address, profileモデルにバリデーションを追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  validates :first_name,
+            :last_name,
+            :first_name_kana,
+            :last_name_kana,
+            :postal_code,
+            :prefectures,
+            :municipalities,
+            :street_number, presence: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,7 @@
 class Profile < ApplicationRecord
+  validates :first_name,
+            :last_name,
+            :first_name_kana,
+            :last_name_kana,
+            :birthday, presence: true
 end
