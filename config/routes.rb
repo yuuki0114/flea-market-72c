ef59d6ccb2b7b9dd8948066b59a2ce8c41e37321
Takuto8786 <iruka8786@gmail.com>
2020-06-22 00:59:35 +0900

    profilesテーブルに情報を登録可能にする

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations",
   }
+  devise_scope :user do
+    get "profiles", to: "users/registrations#new_profile"
+    post "profiles", to: "users/registrations#create_profile"
   root 'items#index'
   resources :users, only: :index do
     collection do
