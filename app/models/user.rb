da6f49aa4ba0c352036b5580597300b9dc21f24e
Takuto8786 <iruka8786@gmail.com>
2020-06-22 13:47:05 +0900

    user情報に追加でバリデーションをかける

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable
   validates :nickname, presence: true
 
+  valid_password_ReGex = /(?=.*?[a-z])(?=.*?\d)[a-z\d]/i
+  validates :password, length: { minimum: 7 }, format: { with: valid_password_ReGex }
+
+  valid_email_ReGex = /\A\S+@\S+\.\S+\z/
+  validates :email, format: { with: valid_email_ReGex }
+
   has_one :address
   has_one :profile
 end
