319d6f1b052f875ae2b3909e36a6c89044a3d8cb
Takuto8786 <iruka8786@gmail.com>
2020-06-22 14:10:51 +0900

    address情報に追加でバリデーションを追加でかける

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,7 +7,19 @@ class Address < ApplicationRecord
             :prefectures,
             :municipalities,
             :street_number, presence: true
+  
+  valid_name_ReGex = /\A[一-龥ぁ-ん]/
+  validates :first_name, :last_name, format: { with: valid_name_ReGex }
 
+  valid_name_kana_ReGex = /\A[ァ-ヶー－]+\z/
+  validates :first_name_kana, :last_name_kana, format: { with: valid_name_kana_ReGex }
+
+  valid_postal_ReGex = /\A\d{3}[-]\d{4}\z/
+  validates :postal_code, format: { with: valid_postal_ReGex }
+
+  valid_phone_ReGex = /\A\d{10,11}\z/
+  validates :telephone_number, format: { with: valid_phone_ReGex }, if: :telephone_number?
+  
   belongs_to :user, optional: true
 
   enum prefectures: {
