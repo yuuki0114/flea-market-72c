514dde0c00daab97fd8f5279bed7ed2ddeed0a9c
Takuto8786 <iruka8786@gmail.com>
2020-06-24 21:25:54 +0900

    それぞれのフォームの下にエラーメッセージ を表示

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ module FleaMarket72c
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      if instance.kind_of?(ActionView::Helpers::Tags::Label)
+        # skip when label
+        html_tag.html_safe
+      else
+        "<div class=\"has-error\">#{html_tag}<span class=\"help-block\">#{instance.error_message.first}</span></div>".html_safe
+      end
+    end
   end
 end
