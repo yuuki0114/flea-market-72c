8bfb9f3dd569f81a9fb0c2b72a8b25eaf64405dc
Takuto8786 <iruka8786@gmail.com>
2020-06-26 12:51:18 +0900

    profileモデルのテストコード記述

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+
+  factory :profile do
+    first_name            {"太郎"}
+    last_name             {"保外"}
+    first_name_kana       {"タロウ"}
+    last_name_kana        {"ホゲ"}
+    birthday              {"1990-10-11"}
+  end
+
+end
