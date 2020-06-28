b5b1388fb501f11f199a86e90d581b9f7367e690
yuuki0114 <y.no0114@gmail.com>
2020-06-28 13:10:50 +0900

    itemコントローラーにcreateアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.images.new
+    @item.build_brand
   end
-  
+
+  def create
+    if Item.create(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end 
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :status, :delivery_fee, :start_address, :shipping_date, :price, brand_attributes: [:name], images_attributes: [:src])
+  end
 end
