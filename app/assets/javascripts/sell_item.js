90dfa2f85af5481213666b291bf561fe26d22c6f
yuuki0114 <y.no0114@gmail.com>
2020-07-03 21:39:17 +0900

    turbolinksに関する部分を削除

--- app/assets/javascripts/sell_item.js
+++ app/assets/javascripts/sell_item.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(function() {
   // 画像用のinputを生成する関数
   const buildFileField = function(index) {
     const html = `<div data-index="${index}" class="js-file_group">
