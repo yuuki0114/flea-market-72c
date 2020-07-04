471b3026115fa68c60cae2062d1201aa9281d848
ryuji0606 <magic.hour006@gmail.com>
2020-07-04 11:38:51 +0900

    コントローラーの設定
    インスタンス変数設定し、モデルからカテゴリーのid
    取得

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @category_parents = Category.where(ancestry: nil)
   end
   
+  #モデルから子カテゴリー取得
+  def category_children
+    @category_children = Category.find(params[:parent_name]).children
+  end
+
+  # モデルから孫カテゴリー取得
+  def category_grandchildren
+    @category_grandchildren = Category.find("#{params[:children_id]}").children
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:category_id,)
+  end
+
 end
