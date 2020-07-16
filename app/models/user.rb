86d2275dd8330bccc32748460a521b146199e449
Takuto8786 <iruka8786@gmail.com>
2020-07-16 18:43:04 +0900

    from_omniauthメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,17 @@ class User < ApplicationRecord
   has_one :credit_card, dependent: :destroy
   has_many  :items
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
