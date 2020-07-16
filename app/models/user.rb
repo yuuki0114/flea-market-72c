a75b2f42aae82ad77b97ce35d9b81d12fb16398c
Takuto8786 <iruka8786@gmail.com>
2020-07-16 18:37:15 +0900

    omniauthのためのオプション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   validates :nickname, presence: true
 
   valid_password_ReGex = /(?=.*?[a-z])(?=.*?\d)[a-z\d]/i
