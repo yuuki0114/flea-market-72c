c88f533f3687444cb9020dcdcf0af8db6329513a
yuuki0114 <y.no0114@gmail.com>
2020-07-24 22:19:28 +0900

    エラーハンドリングの導入

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = "コメントを送信しました。"
+    else
+      flash[:alert] = "コメントを送信できませんでした。"
+    end
     redirect_to  controller: :items, action: :show, id: comment.item_id
   end
 
